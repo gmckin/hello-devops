90270b2758dbc6a1feafd563698bbc364f8a371b
greg mckinney <gmck95@msn.com>
2016-11-30 09:55:14 -0500

    pipeline done [#135064403]

---
+++ Practice/Practice/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practice.Controllers
+{
+  public class HomeController : Controller
+  {
+    // GET: Home
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public JsonResult GetProducts()
+    {
+      var db = new ProductsDBEntities();
+      var products = db.Products.ToList();
+      return Json(products, JsonRequestBehavior.AllowGet);
+    }
+  }
+}
